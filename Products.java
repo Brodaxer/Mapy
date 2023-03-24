e65292d1fc5eefbccbd2931fd3ea388e1bbef543
Brodaxer <mloszewski123@gmail.com>
2023-03-24 14:57:51 +0100

    a

---
+++ Products.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+class Products {
+    private String category;
+    private String name;
+    private double price;
+
+    public Products(String category, String name, double price) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Products products = (Products) o;
+        return Double.compare(products.price, price) == 0 && Objects.equals(category, products.category) && Objects.equals(name, products.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(category, name, price);
+    }
+}
